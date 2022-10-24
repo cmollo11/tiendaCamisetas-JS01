7f5e2c6fdcf0545dc84d2eb0ade4cd29d7975231
cmollo <mollo.cristian@gmail.com>
2022-10-23 21:47:28 -0300

    subo trabajo para primera entrega

---
+++ js/script01.js
@@ -0,0 +1,51 @@
+
+let cupón = 1010;
+
+let inputUsser = prompt("ingrese su cupón de descuento");
+
+if(cupón==inputUsser){
+
+   console.log("cupón ingresado correctamente");
+   alert("cupón ingresado correctamente")
+}
+
+else{
+
+    console.log("su cupón no es válido");
+    alert("su cupón no es válido")
+}
+
+
+let opcion = Number(prompt("Elige tu tipo de Camiseta 1-de Selección 2-Nacionales 3-Gloriosas"));
+
+switch(opcion){
+
+    case 1:
+        alert("Elegiste camisetas de selección");
+        console.log("eligió camisetas de selección");
+    break;
+    
+    case 2:
+        alert("Elegiste camisetas nacionales");
+        console.log("eligió camisetas de nacionales")
+    break;
+
+    case 3:
+        alert("Elegiste camisetas gloriosas")
+        console.log("eligió camisetas gloriosas")
+    break;
+    default:
+        alert("No has seleccionado nada")
+        console.log("opción de camiseta incorrecta")
+    break;
+}
+
+
+let stock = 120;
+
+for(let i=0;i<120;i++){
+
+    console.log("ciclo for: " +i);  
+}
+
+
